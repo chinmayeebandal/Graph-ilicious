4362d37cfc326d01feb4ebeaa0af5b2e59087258
chinbandal <cbandal@rel8ed.to>
2022-04-02 21:06:19 -0400

    Updated code comment docs

--- Sparse.cpp
+++ Sparse.cpp
@@ -1,15 +1,16 @@
 /* *
  * Character: Sparse
- * Represents: Adjacency List 
- *             -> **Vector of Vector -> use for cache friendliness
- *             -> Array of Array Ex: [[1, 2], [2, 3], [3, 1]]
- *                                        index 0| index 1 | index 2  // size == number of nodes
- *                                        node 1| node 2  | node 3
- *             -> Array of List
+ * Weapon: Adjacency List 
+ *             - **Vector of Vector -> use primarily for cache friendliness
+ *             - Array of Array, Ex: [[1, 2], [2, 3], [3, 1]]
+ *                                   index 0| index 1 | index 2  // size == number of nodes
+ *                                    node 1| node 2  | node 3
+ *             - Array of List
  * Properties: 1) array.size() == # of vertices/nodes, 
  *             2) Edge Weights: add a pair of (Node, weight) into adjList
  * Usage: Sparse graphs, aka, small number of edges/ Less connections
- * Challenges
+ * Pros: Saves space O(V+E)
+ * Challenges: Inefficient on Queries like determine if path from node p to node q exists. Takes O(V)
  * 
  * */
 
@@ -45,11 +46,11 @@ struct Graph {
         nodes = std::unordered_set<Node*>();
     }
 
-    //Creates an Edge between two nodes in the graph. makes the graph unidirectional or directed
+    //Creates an Edge between two nodes in the graph. makes the graph directed
     // To add WEIGHT to the Edge, change to List or Vector<Pair<Node*, int>>>, where int is for the weight
     void createEdge(Node* from, Node* to) {
         from->adjList.push_back(to);
-        //Uncommenting the code below makes the graph bi-directional or undirected
+        //Uncommenting the code below makes the graph bi-directional 
         //to->adjList.push_back(from);
     }
 
